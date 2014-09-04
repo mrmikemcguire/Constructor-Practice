adb9d2badb5e142dc82af4d3445e1eda2fe19e93
Mike McGuire <mcguire@mullenhigh.com>
2014-09-04 14:36:45 -0600

    Added for-each loop

--- Constructor Practice - One Class/src/Car.java
+++ Constructor Practice - One Class/src/Car.java
@@ -52,9 +52,9 @@ public class Car
 		 inventory.add(new Car("Mazda", "CX-9", "White", 19.4, 18.7));
 		 inventory.add(new Car("Chevrolet", "Camaro", "Red", 12.3, 16.2));
 		 
-		 for (int i = 0; i < inventory.size(); i++)
+		 for (Car c : inventory)
 			 {
-			 System.out.println(inventory.get(i).getModel());
+			 System.out.println(c.getMpg());
 			 }
 		}
 	}
